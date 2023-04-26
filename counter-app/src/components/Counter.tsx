4c7d04e2b03ae06ca029ed4c70c20a328d9ed439
surajnarsaleindx <snarsale@intelligentdx.com>
2023-04-27 04:10:12 +0530

    refactor: change counter cy file

--- counter-app/src/components/Counter.tsx
+++ counter-app/src/components/Counter.tsx
@@ -1,16 +1,15 @@
-// src/Counter.tsx
 import { useState } from 'react';
 
-function Counter() {
+const Counter = () => {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <button onClick={() => setCount(count - 1)}>-</button>
-      <span>{count}</span>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <button data-testid="decrementButton" onClick={() => setCount(count - 1)}>-</button>
+      <span data-testid="count">{count}</span>
+      <button data-testid="incrementButton" onClick={() => setCount(count + 1)}>+</button>
     </div>
   );
-}
+};
 
 export default Counter;
--- counter-app/src/cypress/e2e/counter.cy.ts
+++ counter-app/src/cypress/e2e/counter.cy.ts
@@ -1,18 +1,17 @@
 describe('Counter', () => {
-    beforeEach(() => {
-      cy.visit('/');
-    });
-  
-    it('increments and decrements count', () => {
-      cy.get('button').contains('-').as('decrementButton');
-      cy.get('button').contains('+').as('incrementButton');
-      cy.get('span').contains('0').as('count');
-  
-      cy.get('@incrementButton').click();
-      cy.get('@count').should('contain', '1');
-  
-      cy.get('@decrementButton').click();
-      cy.get('@count').should('contain', '0');
-    });
+  beforeEach(() => {
+    cy.visit('/');
   });
-  
+
+  it('increments and decrements count', () => {
+    cy.get('[data-testid=decrementButton]').as('decrementButton');
+    cy.get('[data-testid=incrementButton]').as('incrementButton');
+    cy.get('[data-testid=count]').as('count');
+
+    cy.get('@incrementButton').click();
+    cy.get('@count').should('contain', '1');
+
+    cy.get('@decrementButton').click();
+    cy.get('@count').should('contain', '0');
+  });
+});
