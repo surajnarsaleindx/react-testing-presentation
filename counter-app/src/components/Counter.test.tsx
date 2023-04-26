7b806bf3425993dc4700c3d3acaebe630a360935
surajnarsaleindx <snarsale@intelligentdx.com>
2023-04-27 03:38:18 +0530

    feat: add test for coutner

---
+++ counter-app/src/components/Counter.test.tsx
@@ -0,0 +1,17 @@
+// src/Counter.test.tsx
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Counter from './Counter';
+
+test('increments and decrements count', () => {
+  const { getByText } = render(<Counter />);
+  const decrementButton = getByText('-');
+  const incrementButton = getByText('+');
+  const count = getByText('0');
+
+  fireEvent.click(incrementButton);
+  expect(count.textContent).toBe('1');
+
+  fireEvent.click(decrementButton);
+  expect(count.textContent).toBe('0');
+});
