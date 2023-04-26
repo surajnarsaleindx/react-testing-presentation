6abf991262ad3b70ba563b19a4910e6237de9340
surajnarsaleindx <snarsale@intelligentdx.com>
2023-04-27 03:35:18 +0530

    feat: add counter component

---
+++ counter-app/src/components/Counter.tsx
@@ -0,0 +1,16 @@
+// src/Counter.tsx
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <span>{count}</span>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
+}
+
+export default Counter;
